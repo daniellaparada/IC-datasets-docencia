800a61445819234f1e1f0ca8372fc8a5357c4c88
Daniela Laura Parada <profedanielaparada@gmail.com>
2023-07-25 01:13:22 -0300

    elimino temporales

--- fuente/04_visualizacion/fuente.R
+++ fuente/04_visualizacion/fuente.R
@@ -10,60 +10,133 @@ require(tidyverse)
 require(ggfortify)
 require(plotly)
 require(vroom)
-require(sp)
-require(RColorBrewer)
-require(tmap)
-require(sf)
+require(leaflet)
 
 # importo datos de sismos
 sismos <- vroom("sismos_all.csv")
 sismos$Fecha <- as.Date(sismos$Fecha,tryFormats = c("%d/%m/%Y"))
+
+# info de casi 10 años
 min(sismos$Fecha)
 max(sismos$Fecha)
 
-# mapa de argentina
-argentina <- st_read(dsn = "gadm41_ARG_0.shp") %>% st_as_sf()
-provincias <- st_read(dsn = "gadm41_ARG_1.shp") %>% st_as_sf()
-localidades <- st_read(dsn = "gadm41_ARG_2.shp") %>% st_as_sf()
+# dummy para los percibidos
+sismos <- sismos %>% 
+  mutate(Percibido = !is.na(Intensidad),
+         Magnitud = Magn.,
+         Profundidad = Profund.) 
 
-# Verificamos el crs: 4326
-st_crs(provincias)
+# Quitamos las unidades km. en intensidad
+sismos$Profundidad <- as.numeric(str_sub(sismos$Profundidad, 1, -4))
 
-tm_shape(provincias) +
-  tm_fill(col = "lightgray") +
-  tm_text("NAME_1", size = 0.3) +
-  tm_borders(lwd = 1, col = "black")
-
-# primeras pruebas para plotear datos
-sismos_filt <- sismos %>%
-  filter(Intensidad != "NA",
-         Provincia != "OCEANO PACIFICO",
+# Sismos en Argentina Continental
+sismos_arg <- sismos %>%
+  filter(Provincia != "OCEANO PACIFICO",
          Provincia != "CHILE",
          Provincia != "MAULE - CHILE",
          Provincia != "REPÚBLICA DE CHILE",
          Provincia != "REPUBLICA DE CHILE",
          Provincia != "SUR DE CHILE",
-         Provincia != "TFAIAS")
+         Provincia != "TFAIAS",
+         Provincia != "NORTH ISLAND, NEW ZEALAND",
+         Provincia != "REGION NOT FOUND.",
+         Provincia != "PERU",
+         Provincia != "PENINSULA ANTARTICA",
+         Provincia != "PASAJE DE DRAKE",
+         Provincia != "NORTHWEST OF KURIL ISLANDS",
+         Provincia != "NTARTIDA",
+         Provincia != "MAR DE SCOTIA",
+         Provincia != "MAR ARGENTINO",
+         Provincia != "ISLAS SANDWICH DEL SUR",
+         Provincia != "ISLAS SANDWICH",
+         Provincia != "ISLAS SHETLAND",
+         Provincia != "ISLAS SHETLAND DEL SUR",
+         Provincia != "ESTRECHO DE DRAKE",
+         Provincia != "FILIPINAS",
+         Provincia != "I.SANDWICH DEL SUR",
+         Provincia != "ISLAS SANDWICH",
+         Provincia != "ISLAS SHETLAND",
+         Provincia != "ISLAS SHETLAND DEL SUR",
+         Provincia != "ESTRECHO DE DRAKE",
+         Provincia != "SECTOR ANTARTICO",
+         Provincia != "IS. SHETLAND DEL SUR",
+         Provincia != "ISLAS GEORGIA DEL SUR",
+         Provincia != "ISLAS GEORGIAS DEL SUR",
+         Provincia != "ISLAS GEORGIAS y SANDWICH DEL SUR",
+         Provincia != "ISLAS GIORGIA Y SANDWICH DEL SUR",
+         Provincia != "ISLAS ORCADAS",
+         Provincia != "ISLAS ORCADAS DEL SUR",
+         Provincia != "ATLANTICO SUR",
+         Provincia != "BOLIVIA",
+         Provincia != "ISLAS GEORGIAS Y SANDWICH DEL SUR",
+         Provincia != "PARAGUAY",
+         Provincia != "LIM. ARG-CHILE",
+         Provincia != "LIM.ARGENTINA-CHILE",
+         Provincia != "LIM.CHILE-ARGENTINA",
+         Provincia != "LIMITE ARGENTINA-CHILE",
+         Provincia != "LIMITE ARGENTINA CHILE",
+         Provincia != "TIERRA DEL FUEGO")
 
-table(sismos_filt$Provincia)
+table(sismos_arg$Provincia)
 
-sismos_pos <- st_as_sf(x=sismos_filt,
-                     coords = c("Longitud" ,"Latitud"),
-                     crs = 4326)
+pal1 <- colorNumeric(palette = c("deeppink2", "deeppink4"), domain = sismos_arg$Profundidad)
+pal2 <- colorNumeric(palette = c("dodgerblue2", "dodgerblue4"), domain = sismos_arg$Profundidad)
 
-tm_shape(provincias) +
-  tm_fill(col = "lightgray") +
-  tm_text("NAME_1", size = 0.3) +
-  tm_borders(lwd = 1, col = "black") +
-  tm_shape(sismos_pos) + 
-  tm_dots(size = 0.1)
+mapa_arg <- sismos_arg %>% 
+  leaflet(options = leafletOptions(attributionControl=FALSE))%>%
+  addTiles(group = "OSM (default)") %>%
+  addProviderTiles("OpenTopoMap", group = "Topográfico",
+                   options = providerTileOptions(opacity=0.5)) %>%
+  addCircleMarkers(data=sismos_arg%>% 
+                     filter(is.na(Intensidad)),
+                   lng= ~Longitud, 
+                   lat= ~Latitud,
+                   fillOpacity=0.1,
+                   radius=~Magnitud,
+                   color = ~pal2(Profundidad),
+                   stroke = FALSE, 
+                   group = "Sismos no percibidos") %>% 
+  addCircleMarkers(data=sismos_arg%>% 
+               filter(Intensidad != "NA"),
+             lng= ~Longitud, 
+             lat= ~Latitud,
+             fillOpacity=0.1,
+             radius=~Magnitud,
+             color = ~pal1(Profundidad),
+             stroke = FALSE, 
+             group = "Sismos percibidos") %>% 
+  addLayersControl(baseGroups = c("OSM (default)", "Topográfico"),
+                   overlayGroups = c("Sismos no percibidos", "Sismos percibidos"),
+                   options = layersControlOptions(collapsed = TRUE))
+mapa_arg
 
-m <- #tm_shape(provincias) +
-  #tm_fill(col = "white") +
-  #tm_text("NAME_1", size = 0.3) +
-  #tm_borders(lwd = 1, col = "black") +
-  tm_shape(sismos_pos) + 
-  tm_dots(size = 0.1)
+mapa_arg_perc <- sismos_arg_perc %>% 
+  leaflet(options = leafletOptions(attributionControl=FALSE))%>%
+  addProviderTiles("OpenStreetMap",
+                   options = providerTileOptions(opacity=1)) %>%
+  addCircleMarkers(lng= ~Longitud, 
+                   lat= ~Latitud,
+                   fillOpacity=0.15,
+                   radius=~Magnitud,
+                   color = ~pal(Profundidad),
+                   stroke = FALSE) 
+mapa_arg_perc
 
-tmap_leaflet(m)
+# Sismos percibidos en San Juan
+sismos_SJ_perc <- sismos_arg_perc %>%
+  filter(Provincia == "SAN JUAN")
+
+mapa_SJ_perc <- sismos_SJ_perc %>% 
+  leaflet(options = leafletOptions(attributionControl=FALSE))%>%
+  addProviderTiles("OpenStreetMap",
+                   options = providerTileOptions(opacity=1)) %>%
+  addCircleMarkers(lng= ~Longitud, 
+                   lat= ~Latitud,
+                   fillOpacity=0.25,
+                   radius=~Magnitud,
+                   color = ~pal(Profundidad),
+                   stroke = FALSE) 
+mapa_SJ_perc
+
+# Sismos en San Juan
 
